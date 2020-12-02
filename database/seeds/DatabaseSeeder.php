4ebd96c7407ee9b77a7dc63e01e9d750e4f7a6aa
Shreyash Patel <shreyashcoderkube@gmail.com>
2020-12-02 20:16:36 +0530

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        // $this->call(EmotionSeeder::class);
+        $this->call(EmotionSeeder::class);
         $this->call(SubEmotionSeeder::class);
     }
 }
